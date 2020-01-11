84dc3f79d302f929e6ffa511eff85faa5411d3c7
Ubuntu <ubuntu@ip-172-31-25-71.ec2.internal>
2020-01-11 23:11:57 +0000

    updated code

--- code.py
+++ code.py
@@ -122,7 +122,7 @@ def mt_splash_scene():
         # update game logic
 
         # Wait for 1 seconds
-        time.sleep(0.01)
+        time.sleep(3.0)
         game_splash_scene()
 
         # redraw sprite list
@@ -191,13 +191,14 @@ def game_splash_scene():
     text1.text("SNAKOB STUDIOS")
     text.append(text1)
     
-    stars = []
-    while True:
-        time.sleep(1.0)
-        stars = []
-        for cloud_number in range(constants.STAR_NUMBER):
-            star = stage.Sprite(image_bank_3, 15, random.randint(0, 160),random.randint(0, 128))
-            stars.append(star)
+    # get sound ready
+    # follow this guide to convert your other sounds to something that will work
+    #    https://learn.adafruit.com/microcontroller-compatible-audio-file-conversion
+    snake_sound = open("1234.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(True)
+    sound.play(snake_sound)
 
     game = stage.Stage(ugame.display, 60)
 
@@ -207,7 +208,7 @@ def game_splash_scene():
     while True:
         # get user input
         # update game logic
-        time.sleep(100.0)
+        time.sleep(4.0)
         main_menu_scene()
         # redraw sprite list
         game.render_sprites(snakob)
@@ -256,11 +257,16 @@ def main_menu_scene():
 
     text = []
 
-    text1 = stage.Text(width=29, height=14, font=None, palette=constants.CUSTOM_PALETTE, buffer=None)
-    text1.move(35, 40)
-    text1.text("PRESS START")
+    text1 = stage.Text(width=50, height=30, font=None, palette=constants.CUSTOM_PALETTE, buffer=None)
+    text1.move(23, 30)
+    text1.text("SNAKOB'S FOREST")
     text.append(text1)
-
+    
+    text2 = stage.Text(width=29, height=14, font=None, palette=constants.CUSTOM_PALETTE, buffer=None)
+    text2.move(37, 55)
+    text2.text("PRESS START")
+    text.append(text2)
+    
     # create a stage for the background to show up on
     #   and set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
@@ -276,31 +282,283 @@ def main_menu_scene():
         game.layers = text + sprites + stars + [background]
         game.render_block()
 
+    # update game logic
+        keys = ugame.buttons.get_pressed()
+        #print(keys)
+        if keys & ugame.K_START != 0:  # Start button
+            game_scene()
+            #break
+
 def game_scene():
-    # this function is the game scene
+    def show_snakes():
+        # I know this is a function that is using variables outside of itself!
+        #   BUT this code is going to be used in 2 places :)
+        # make an alien show up on screen in the x-axis
+        for snakob_number in range(len(snakes)):
+            if snakes[snake_number].x < 0: # meaning it is off the screen, so available to move on the screen
+                snakes[snake_number].move(random.randint(0 + constants.SPRITE_SIZE, constants.SCREEN_X - constants.SPRITE_SIZE), constants.OFF_TOP_SCREEN)
+                break
+
+    # buttons that you want to keep state information on
+    a_button = constants.button_state["button_up"]
+    b_button = constants.button_state["button_up"]
+    start_button = constants.button_state["button_up"]
+    select_button = constants.button_state["button_up"]
+
+    # get sound ready
+    pew_sound = open("pew2.wav", 'rb')  # to change the wav volume: https://audioalter.com/volume/
+    boom_sound = open("boom.wav", 'rb')
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+
+    # an image bank for CircuitPython
+    image_bank_1 = stage.Bank.from_bmp16("SPRITES.bmp")
+    # a list of sprites that will be updated every frame
+    sprites = []
+    
+    image_bank_5 = stage.Bank.from_bmp16("background_for_game_scene.bmp")
+    
+    background_for_game_scene = []
+
+    # create lasers for when we shoot
+    rocks = []
+    for rock_number in range(constants.TOTAL_NUMBER_OF_ROCKS):
+        a_single_rock = stage.Sprite(image_bank_1, 15, constants.OFF_SCREEN_X, constants.OFF_SCREEN_X)
+        rocks.append(a_single_rock)
+
+    # create aliens
+    snakes = []
+    for snake_number in range(constants.TOTAL_NUMBER_OF_SNAKES):
+        a_single_snake = stage.Sprite(image_bank_1, 7, constants.OFF_SCREEN_X, constants.OFF_SCREEN_X)
+        snakes.append(a_single_snake)
+
+    # current number of aliens that should be moving down screen, start with just 1
+    snake_count = 1
+    show_snakes()
+
+    # add text at top of screen for score
+
+    snakob = stage.Sprite(image_bank_1, 5, int(constants.SCREEN_X / 2), constants.SCREEN_Y - constants.SPRITE_SIZE)
+    sprites.append(snakob) # insert at the top of sprite list
+
+    # sets the background to image 0 in the bank
+    background = stage.Grid(image_bank_1, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            tile_picked = random.randint(13, 15)
+            background.tile(x_location, y_location, tile_picked)
+
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+    # set the layers, items show up in order
+    game.layers = sprites + rocks + snakob + [score_text] + [background]
+    # render the background and inital location of sprite list
+    # most likely you will only render background once per scene
+    game.render_block()
 
     # repeat forever, game loop
     while True:
         # get user input
+        keys = ugame.buttons.get_pressed()
+        #print(keys)
+
+        if keys & ugame.K_X != 0:  # A button
+            if a_button == constants.button_state["button_up"]:
+                a_button = constants.button_state["button_just_pressed"]
+            elif a_button == constants.button_state["button_just_pressed"]:
+                a_button = constants.button_state["button_still_pressed"]
+        else:
+            if a_button == constants.button_state["button_still_pressed"]:
+                a_button = constants.button_state["button_released"]
+            else:
+                a_button = constants.button_state["button_up"]
 
         # update game logic
 
-        # redraw sprite list
-        pass # just a placeholder until you write the code
+        # if right D-Pad is pressed
+        if keys & ugame.K_RIGHT != 0:
+            # if ship moves off right screen, move it back
+            if snakob.x > constants.SCREEN_X - constants.SPRITE_SIZE:
+                snakob.x = constants.SCREEN_X - constants.SPRITE_SIZE
+            # else move ship right
+            else:
+                snakob.move(snakob.x + constants.SNAKOB_SPEED, snakob.y)
 
+        # if left D-Pad is pressed
+        if keys & ugame.K_LEFT != 0:
+            # if ship moves off left screen, move it back
+            if snakob.x < 0:
+                snakob.x = 0
+            # else move ship left
+            else:
+                snakob.move(ship.x - constants.SNAKOB_SPEED, ship.y)
+
+        # if A Button (fire) is pressed
+        if a_button == constants.button_state["button_just_pressed"]:
+            # fire a laser, if we have enough power (meaning we have not used up all the lasers)
+            for rock_number in range(len(lasers)):
+                if rocks[rocks_number].x < 0:
+                    rocks[rocks_number].move(snakob.x, snakob.y)
+                    sound.stop()
+                    sound.play(pew_sound)
+                    break
+
+        if b_button == constants.button_state["button_still_pressed"]:
+            # fire a laser, if we have enough power (meaning we have not used up all the lasers)
+            for rock_number in range(len(rocks)):
+                if rocks[rock_number].x < 0:
+                    rocks[rock_number].move(snakob.x, snakob.y)
+                    sound.stop()
+                    sound.play(pew_sound)
+                    break
+
+        # each frame move the lasers, that have been fired, up
+
+        # first make all the neopixels yellow, then make them green if it is moving up
+        rocks_moving_counter = -1
+        for pixel_number in range(0, 5):
+            pixels[pixel_number] = (0, 10, 0)
+
+        for rock_number in range(len(rocks)):
+            if rocks[rock_number].x > 0:
+                rocks[rock_number].move(rocks[rock_number].x, rocks[rock_number].y - constants.ROCK_SPEED)
+                rocks_moving_counter = rocks_moving_counter + 1
+                pixels[rocks_moving_counter] = (10, 10 - (2 * rocks_moving_counter + 2), 0)
+                if rocks[rock_number].y < constants.OFF_TOP_SCREEN:
+                    rocks[rock_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+        if rocks_moving_counter == 4:
+            for pixel_number in range(0, 5):
+                pixels[pixel_number] = (10, 0, 0)
+        pixels.show()
+
+        # each frame move the aliens down the screen
+        for snake_number in range(len(snakes)):
+            if snakes[snake_number].x > 0: # meaning it is on the screen
+                snakes[snake_number].move(snakes[snake_number].x, snakes[snake_number].y + constants.SNAKE_SPEED)
+                if snakes[snake_number].y > constants.SCREEN_Y:
+                    snakes[snake_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+                    show_snakes() # make it randomly show up at top again
+
+        # each frame check if any of the lasers are touching any of the aliens
+        for rock_number in range(len(rocks)):
+            if rocks[rock_number].x > 0 :
+                for snake_number in range(len(snakes)):
+                    if snakes[snake_number].x > 0:
+                        # https://circuitpython-stage.readthedocs.io/en/latest/#stage.collide
+                        # and https://stackoverflow.com/questions/306316/determine-if-two-rectangles-overlap-each-other
+
+score_text = []
+    
+    score_text = stage.Text(width=29, height=14, font=None, palette=constants.SCORE_PALETTE, buffer=None)
+    score_text.text("Score: {0}".format(score))
+    score_text.clear()
+    score_text.cursor(0, 0)
+    score_text.move(1, 1)
+    
+                        # the first 4 numbers are the coordinates of A box
+                        # since the laser is thin, it made it thinner and slightly smaller
+                        #
+                        # the second 4 numbers are the alien, it is more of a box so I just made it slightly smaller
+                        #
+                        # if you slow down the FPS, then you can see the interaction more easily to alter these numbers
+                        if stage.collide(rocks[rock_number].x + 6, rock[rock_number].y + 2,
+                                         rocks[rock_number].x + 11, rocks[rock_number].y + 12,
+                                         snakes[snake_number].x + 1, snakes[snake_number].y,
+                                         snakes[snake_number].x + 15, snakes[snake_number].y + 15):
+                            # you hit an alien
+                            snakes[snake_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+                            rocks[rock_number].move(constants.OFF_SCREEN_X, constants.OFF_SCREEN_Y)
+                            # add 1 to the score
+                            score += 1
+                            score_text.clear()
+                            score_text.cursor(0, 0)
+                            score_text.move(1, 1)
+                            score_text.text("Score: {0}".format(score))
+                            # this will freeze the screen for a split second, but we have no option
+                            game.render_block()
+                            # play sound effect
+                            sound.stop()
+                            sound.play(boom_sound)
+                            show_snake()
+                            show_snake()
+                            snake_count = snake_count + 1
+
+        # each frame check if any of the aliens are touching the ship
+        for snake_number in range(len(snakes)):
+            if snakes[snake_number].x > 0:
+                # https://circuitpython-stage.readthedocs.io/en/latest/#stage.collide
+                # and https://stackoverflow.com/questions/306316/determine-if-two-rectangles-overlap-each-other
+                if stage.collide(snakes[snake_number].x + 1, snakes[snake_number].y,
+                                 snakes[snake_number].x + 15, snakes[snake_number].y + 15,
+                                 snakob.x, snakob.y,
+                                 snakob.x + 15, snakob.y + 15):
+                    # alien hit the ship
+                    sound.stop()
+                    sound.play(crash_sound)
+                    for pixel_number in range(0, 5):
+                        pixels[pixel_number] = (25, 0, 25)
+                    pixels.show()
+                    # Wait for 1 seconds
+                    time.sleep(4.0)
+                    # need to release the NeoPixels
+                    pixels.deinit()
+                    sound.stop()
+                    game_over_scene(score)
+
+        # redraw sprite list
+        game.render_sprites(sprites + rocks + snakes)
+        game.tick() # wait until refresh rate finishes
 
 def game_over_scene(final_score):
     # this function is the game over scene
+    # an image bank for CircuitPython
+    image_bank_2 = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # sets the background to image 0 in the bank
+    background = stage.Grid(image_bank_2, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
+
+    text = []
+
+    text0 = stage.Text(width=29, height=14, font=None, palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text0.move(22, 20)
+    text0.text("Final Score: {:0>2d}".format(final_score))
+    text.append(text0)
+
+    text1 = stage.Text(width=29, height=14, font=None, palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text1.move(43, 60)
+    text1.text("GAME OVER")
+    text.append(text1)
+
+    text2 = stage.Text(width=29, height=14, font=None, palette=constants.MT_GAME_STUDIO_PALETTE, buffer=None)
+    text2.move(32, 110)
+    text2.text("PRESS SELECT")
+    text.append(text2)
+
+    # create a stage for the background to show up on
+    #   and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+    # set the layers, items show up in order
+    game.layers = text + [background]
+    # render the background and inital location of sprite list
+    # most likely you will only render background once per scene
+    game.render_block()
 
     # repeat forever, game loop
     while True:
         # get user input
 
         # update game logic
+        keys = ugame.buttons.get_pressed()
+        #print(keys)
+
+        if keys & ugame.K_SELECT != 0:  # Start button
+            keys = 0
+            menu_scene()
+            #break
 
         # redraw sprite list
-        pass # just a placeholder until you write the code
-
 
 if __name__ == "__main__":
-    blank_white_reset_scene()
+    mt_splash_scene()
